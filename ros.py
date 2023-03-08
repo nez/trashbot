eaf13310ca9070c23d5bd0747c6e284c4dc46e6e
Gu Andres <andres@robotani.ca>
2023-03-08 12:10:19 -0600

    ros.py

--- ros.py
+++ ros.py
@@ -16,6 +16,7 @@ from scipy.ndimage import filters
 import cv2
 import roslib
 import rospy
+import fastai
 
 # Ros Messages
 from sensor_msgs.msg import CompressedImage
@@ -36,48 +37,32 @@ class image_feature:
         self.subscriber = rospy.Subscriber("/usb_cam/image_raw/compressed",
             CompressedImage, self.callback,  queue_size = 1)
         if VERBOSE :
-            print "subscribed to /usb_cam/image_raw/compressed"
+            print("subscribed to /usb_cam/image_raw/compressed")
 
     def callback(self, ros_data):
         '''Callback function of subscribed topic.
         Here images get converted and features detected'''
         if VERBOSE :
-            print 'received image of type: "%s"' % ros_data.format
+            print('received image of type: "%s"' % ros_data.format)
 
         #### direct conversion to CV2 ####
         np_arr = np.fromstring(ros_data.data, np.uint8)
-        image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
-        #image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
+        print(np_arr)
+        print(len(np_arr))
+        #image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
+        print(len(image_np))
 
-        #### Feature detectors using CV2 ####
-        # "","Grid","Pyramid" +
-        # "FAST","GFTT","HARRIS","MSER","ORB","SIFT","STAR","SURF"
-        method = "GridFAST"
-        feat_det = cv2.FeatureDetector_create(method)
-        time1 = time.time()
-
-        # convert np image to grayscale
-        featPoints = feat_det.detect(
-            cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY))
-        time2 = time.time()
-        if VERBOSE :
-            print '%s detector found: %s points in: %s sec.'%(method,
-                len(featPoints),time2-time1)
-
-        for featpoint in featPoints:
-            x,y = featpoint.pt
-            cv2.circle(image_np,(int(x),int(y)), 3, (0,0,255), -1)
-
-        cv2.imshow('cv_img', image_np)
-        cv2.waitKey(2)
+        #cv2.imshow('cv_img', image_np)
+        #cv2.waitKey(2)
 
         #### Create CompressedIamge ####
-        msg = CompressedImage()
-        msg.header.stamp = rospy.Time.now()
-        msg.format = "jpeg"
-        msg.data = np.array(cv2.imencode('.jpg', image_np)[1]).tostring()
+        #msg = CompressedImage()
+        #msg.header.stamp = rospy.Time.now()
+        #msg.format = "jpeg"
+        #msg.data = np.array(cv2.imencode('.jpg', image_np)[1]).tostring()
         # Publish new image
-        self.image_pub.publish(msg)
+        #self.image_pub.publish(msg)
 
         #self.subscriber.unregister()
 
@@ -88,7 +73,7 @@ def main(args):
     try:
         rospy.spin()
     except KeyboardInterrupt:
-        print "Shutting down ROS Image feature detector module"
+        print("Shutting down ROS Image feature detector module")
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
