511f1645c3efe76ee9c2320b05de39e15f5acb43
Gu Andres <andres@robotani.ca>
2023-04-06 17:47:54 -0600

    trashbot update

---
+++ fastros-lite-object-detect.py
@@ -0,0 +1,114 @@
+import sys, time
+import numpy as np
+from scipy.ndimage import filters
+import cv2
+import roslib
+import rospy
+from fastcore.all import *
+from fastdownload import download_url
+from fastai.vision.all import *
+from fastai.data.external import *
+from time import sleep
+import timm
+from sensor_msgs.msg import CompressedImage
+from hassapi import Hass
+
+# https://answers.ros.org/question/330654/how-to-connect-remote-roscore-with-python-in-runtime/
+
+VERBOSE=False
+
+learn = load_learner('trashsort-lite.pkl')
+
+processing = False
+
+def predict_trash(img):
+    color_coverted = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    pil_image = Image.fromarray(color_coverted)
+    what,_,probs = learn.predict(pil_image)#.reshape(192,192)
+    #print(f"Probs len: {len(probs)}")
+    print(what)
+    return what
+
+
+
+
+
+from imageai.Detection import ObjectDetection
+import os
+execution_path = os.getcwd()
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath( os.path.join(execution_path , "/home/nex/ML-notebooks/ImageAI/yolov3.pt"))
+detector.loadModel()
+def detect(img):
+    return detector.detectObjectsFromImage(img, minimum_percentage_probability=30)
+
+def detect_trash(img):
+    objects = detect
+try:
+    hass = Hass(hassurl="http://192.168.1.64:8123/", token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1OWI2YzM5MjE3NWY0YWMwOWM2NmY0MDcwZDdiY2FkYSIsImlhdCI6MTY3ODIyNTkxOSwiZXhwIjoxOTkzNTg1OTE5fQ.1eHXMscPPD1S8q9cCUnyN2BZ5wP5BMtSMqZ8s3zzgMA")
+except:
+    print("could not connect to hass")
+
+def predictor(img):
+    prediction = predict_trash(img)
+    if prediction == 'Cigarette':
+        print("Cigarette detected!!!")
+        hass.turn_on("switch.aspirator")
+        time.sleep(1)
+    else:
+        hass.turn_off("switch.aspirator")
+
+class image_feature:
+    def __init__(self):
+        '''Initialize ros publisher, ros subscriber'''
+        # topic where we publish
+        self.image_pub = rospy.Publisher("/output/image_raw/compressed",
+            CompressedImage)
+
+        # subscribed Topic
+        self.subscriber = rospy.Subscriber("/usb_cam/image_raw/compressed",
+                                           CompressedImage, self.callback,  queue_size = 1, buff_size=2**29)
+        if VERBOSE :
+            print("subscribed to /usb_cam/image_raw/compressed")
+
+    def callback(self, ros_data):
+        '''Callback function of subscribed topic.
+        Here images get converted and features detected'''
+        if VERBOSE :
+            print('received image of type: "%s"' % ros_data.format)
+        #### direct conversion to CV2 ####
+        np_arr = np.fromstring(ros_data.data, np.uint8)
+        #print(np_arr)
+        #print(len(np_arr))
+        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
+        #print(image_np.shape)
+        #print(len(image_np))
+        #print(predict_trash(image_np))
+        predictor(image_np)
+        #cv2.imshow('cv_img', image_np)
+        #cv2.waitKey(2)
+
+        #### Create CompressedIamge ####
+        msg = CompressedImage()
+        msg.header.stamp = rospy.Time.now()
+        msg.format = "jpeg"
+        msg.data = np.array(cv2.imencode('.jpg', image_np)[1]).tostring()
+        # Publish new image
+        self.image_pub.publish(msg)
+
+        #self.subscriber.unregister()
+
+
+def main(args):
+    '''Initializes and cleanup ros node'''
+    ic = image_feature()
+    rospy.init_node('image_feature', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down ROS Image feature detector module")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ ros_diffdrive_robot/ros_diffdrive_robot.ino
@@ -0,0 +1,222 @@
+/*
+
+  Arduino code to control a differential drive robot via ROS Twist messages using a NodeMcu or ESP8266
+  If you have questions or improvements email me at reinhard.sprung@gmail.com
+
+  Launch a ros serial server to connect to:
+    roslaunch rosserial_server socket.launch
+
+  Launch a teleop gamepad node:
+    roslaunch teleop_twist_joy teleop.launch joy_config:="insert gamepad type"
+
+
+  MIT License
+
+  Copyright (c) 2018 Reinhard Sprung
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+#include <math.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+
+#define LED_BUILTIN 2 // Remapping the built-in LED since the NodeMcu apparently uses a different one.
+#define LED_BUILTIN_RED 16 // If using a NodeMcu v1, then there's another red onboard led.
+// The min amount of PWM the motors need to move. Depends on the battery, motors and controller.
+// The max amount is defined by PWMRANGE in Arduino.h
+#define PWM_MIN 300
+
+// Declare functions
+void setupPins();
+void setupSerial();
+void setupWiFi();
+bool rosConnected();
+void onTwist(const geometry_msgs::Twist &msg);
+float mapPwm(float x, float out_min, float out_max);
+
+// Pins
+const uint8_t R_PWM = D1;
+const uint8_t R_BACK = D2;
+const uint8_t R_FORW = D3;
+const uint8_t L_BACK = D5;
+const uint8_t L_FORW = D6;
+const uint8_t L_PWM = D7;
+
+// Wifi
+// If access point is defined, a Wifi network with this name will be created.
+// Remove if you want to connect to an existing network.
+#define ACCESS_POINT_SSID "SMARTCAR"
+
+#ifndef ACCESS_POINT_SSID
+ESP8266WiFiMulti wifi;
+#endif
+
+// ROS serial server
+IPAddress server(192, 168, 1, 73);
+ros::NodeHandle node;
+ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &onTwist);
+
+bool _connected = false;
+
+void setup()
+{
+  setupPins();
+  setupSerial();
+  setupWiFi();
+
+  // Connect to rosserial socket server and init node. (Using default port of 11411)
+  Serial.printf("Connecting to ROS serial server at %s\n", server.toString().c_str());
+  node.getHardware()->setConnection(server);
+  node.initNode();
+  node.subscribe(sub);
+}
+
+void setupPins()
+{
+  // Status LED
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  pinMode(L_PWM, OUTPUT);
+  pinMode(L_FORW, OUTPUT);
+  pinMode(L_BACK, OUTPUT);
+  pinMode(R_PWM, OUTPUT);
+  pinMode(R_FORW, OUTPUT);
+  pinMode(R_BACK, OUTPUT);
+  stop();
+}
+
+void setupSerial()
+{
+  Serial.begin(115200);
+  Serial.println();
+}
+
+void setupWiFi()
+{
+#ifdef ACCESS_POINT_SSID
+
+  WiFi.disconnect();
+  Serial.println("Creating Wifi network");
+  if (WiFi.softAP(ACCESS_POINT_SSID))
+  {
+    Serial.println("Wifi network created");
+    Serial.print("SSID: ");
+    Serial.println(WiFi.softAPSSID());
+    Serial.print("IP:   ");
+    Serial.println(WiFi.softAPIP());
+  }
+
+#else
+
+  WiFi.softAPdisconnect();
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  wifi.addAP("INFINITUM5E69", "BjVvCpz26h");
+  wifi.addAP("--wifi_2--", "--password_2--");
+  wifi.addAP("--wifi_3--", "--password_3--");
+
+  Serial.println("Connecting to Wifi");
+  while (wifi.run() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi connected");
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  Serial.print("IP:   ");
+  Serial.println(WiFi.localIP());
+
+#endif
+}
+
+void stop()
+{
+  digitalWrite(L_FORW, 0);
+  digitalWrite(L_BACK, 0);
+  digitalWrite(R_FORW, 0);
+  digitalWrite(R_BACK, 0);
+  analogWrite(L_PWM, 0);
+  analogWrite(R_PWM, 0);
+}
+
+void onTwist(const geometry_msgs::Twist &msg)
+{
+  int PWMRANGE = 1023;
+  if (!_connected)
+  {
+    stop();
+    return;
+  }
+
+  // Cap values at [-1 .. 1]
+  float x = max(min(msg.linear.x, 1.0f), -1.0f);
+  float z = max(min(msg.angular.z, 1.0f), -1.0f);
+
+  // Calculate the intensity of left and right wheels. Simple version.
+  // Taken from https://hackernoon.com/unicycle-to-differential-drive-courseras-control-of-mobile-robots-with-ros-and-rosbots-part-2-6d27d15f2010#1e59
+  float l = (msg.linear.x - msg.angular.z) / 2;
+  float r = (msg.linear.x + msg.angular.z) / 2;
+
+  // Then map those values to PWM intensities. PWMRANGE = full speed, while PWM_MIN = the minimal amount of power at which the motors begin moving.
+  uint16_t lPwm = mapPwm(fabs(l), PWM_MIN, PWMRANGE);
+  uint16_t rPwm = mapPwm(fabs(r), PWM_MIN, PWMRANGE);
+
+  // Set direction pins and PWM
+  digitalWrite(L_FORW, l > 0);
+  digitalWrite(L_BACK, l < 0);
+  digitalWrite(R_FORW, r > 0);
+  digitalWrite(R_BACK, r < 0);
+  analogWrite(L_PWM, lPwm);
+  analogWrite(R_PWM, rPwm);
+}
+
+void loop()
+{
+  if (!rosConnected())
+    stop();
+  node.spinOnce();
+}
+
+bool rosConnected()
+{
+  // If value changes, notify via LED and console.
+  bool connected = node.connected();
+  digitalWrite(LED_BUILTIN, false);
+  Serial.println(connected);
+  if (_connected != connected)
+  {
+    _connected = connected;
+    digitalWrite(LED_BUILTIN, !connected); // false -> on, true -> off
+    Serial.println(connected ? "ROS connected" : "ROS disconnected");
+  }
+  return connected;
+}
+
+// Map x value from [0 .. 1] to [out_min .. out_max]
+float mapPwm(float x, float out_min, float out_max)
+{
+  return x * (out_max - out_min) + out_min;
+}
